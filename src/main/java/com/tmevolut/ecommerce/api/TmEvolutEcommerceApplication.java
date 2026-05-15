6d436ae30bc6d6b24e346061ab55d3e5f34b50e1
Herbert Taylor <taylortech2024@gmail.com>
2026-05-15 14:46:45 -0300

    fix: adiciona classe principal da aplicacao

---
+++ src/main/java/com/tmevolut/ecommerce/api/TmEvolutEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.tmevolut.ecommerce.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmEvolutEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmEvolutEcommerceApplication.class, args);
+    }
+
+}
